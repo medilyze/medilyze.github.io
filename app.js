22a538214c033331d9eeb4b12583d6e9d2870850
Ethan Harvey <Ethan.Harvey@tufts.edu>
2024-11-16 12:49:25 -0500

    Updated margins

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ function includeHTML() {
     var z, i, elmnt, file, xhttp;
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      file = elmnt.getAttribute("html");
-      if (file) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-            if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-            elmnt.removeAttribute("html");
-            includeHTML();
-          }
+        elmnt = z[i];
+        file = elmnt.getAttribute("html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                elmnt.removeAttribute("html");
+                includeHTML();
+            }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        return;
-      }
     }
 };
 
